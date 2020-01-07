76581233d082979657d3f1f377af9df82adba1f1
Amit <ajbendor@gmail.com>
2020-01-07 18:18:45 +0200

    little improvements to PWA

---
+++ lms/templates/static_templates/pwa-sw-register.js
@@ -0,0 +1,21 @@
+// This is the service worker with the Cache-first network
+
+// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+
+// Check compatibility for the browser we're running this in
+if ("serviceWorker" in navigator) {
+    if (navigator.serviceWorker.controller) {
+      console.log("active service worker found, no need to register");
+    } else {
+      // Register the service worker
+      navigator.serviceWorker
+        .register("pwa-sw.js", {
+          scope: "./"
+        })
+        .then(function (reg) {
+          console.log("Service worker has been registered for scope: " + reg.scope);
+        });
+    }
+  }
+
+  
